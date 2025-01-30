018cf63217ea338a1119e278d7eff430d8ae0c49
ThomaFM <72629297+ThomaFM@users.noreply.github.com>
2025-01-30 10:09:56 -0500

    Update minkowskiSum.h

--- content/geometry/minkowskiSum.h
+++ content/geometry/minkowskiSum.h
@@ -1,35 +1,26 @@
 /**
- * Description: returns the minkowski sum of several polygons
- * Status: Tested on USACO 2022 January, Platinum: Multiple Choice Test.
+ * Description: Returns the minkowski sum of a set of convex polygons
+ * Time: O(n \log n)
+ * Author: Tyler Marks
+ * Source: CP-Algorithms 
  */
-
-template<class P> vector<P> minkSum(vector<vector<P>> &polys){
-	P init(0, 0);
-	vector<P> dir;
+#define side(p) (p.x > 0 || (p.x == 0 && p.y > 0))
+template<class P> 
+vector<P> convolve(vector<vector<P>> &polys){
+	P init; vector<P> dir;
 	for(auto poly: polys) {
 		int n = sz(poly);
-		if(n == 0)
-			continue;
-		init = init + poly[0];
-		if(n == 1)
-			continue;
-		rep(i, 0, n)
-			dir.push_back(poly[(i+1)%n] - poly[i]);
+		if(n) init = init + poly[0];
+		if(n < 2) continue;
+		rep(i, 0, n) dir.push_back(poly[(i+1)%n] - poly[i]);
 	}
-	if(size(dir) == 0)
-		return {init};
-	sort(all(dir), [&](P a, P b)->bool {
-		bool sideA = a.x > 0 || (a.x == 0 && a.y > 0);
-		bool sideB = b.x > 0 || (b.x == 0 && b.y > 0);
-		if(sideA != sideB)
-			return sideA;
+	if(size(dir) == 0) return { init };
+	stable_sort(all(dir), [&](P a, P b)->bool {
+		if(side(a) != side(b)) return side(a);
 		return a.cross(b) > 0;
 	});
-	vector<P> sum;
-	P cur = init;
-	rep(i, 0, sz(dir)) {
-		sum.push_back(cur);
-		cur = cur + dir[i];
-	}
+	vector<P> sum; P cur = init;
+	rep(i, 0, sz(dir))
+		sum.push_back(cur), cur = cur + dir[i];
 	return sum;
 }

27b8fb91f86be331092e27c5170097b08887135c
Thomas Meeks <thomasfm2004@gmail.com>
2025-10-01 16:23:11 -0400

    adding suffix array query structure

--- content/strings/SuffixArrayQuery.h
+++ content/strings/SuffixArrayQuery.h
@@ -4,28 +4,30 @@
  * License: CC0
  * Source: Luke Videckis
  * Description: Various helper queries for suffix array problems
- * inputs are 0-based and ranges are given and returned as inc-ex
- * Time: len_lcp: O(1), cmp_sub: O(1), find_str: O(log(n))
+ * inputs are 0based and ranges are given and returned as incex
+ * Time: lenlcp: O(1), cmpsub: O(1), findstr: O(log(n))
  * Status: Tested
 */
+#pragma once
+
 struct SAQuery{
     SuffixArray sa; RMQ<int> lcp;
     string s; vector<int> sainv;
-    SAQuery(SuffixArray sa,string s): sa(sa), lcp(sa.lcp), s(s){
+    SAQuery(SuffixArray sa,string s):sa(sa), lcp(sa.lcp), s(s){
         sainv.resize(sz(s)+1);
         rep(i,0,sz(sa.sa)) sainv[sa.sa[i]] = i;
-    } //len_lcp: takes in two starting ind's and returns lcp
+    } //len_lcp: takes in two ind and returns lcp
     int len_lcp(int u, int v){
         if(u==v) return sz(s)-u;
         auto[l,r] = minmax(sainv[u],sainv[v]);
         return lcp.query(l+1,r+1);
-    } //given [) substrings of s: s1<s2->-1,s1==s2->0,s1>s2->1
+    } //given [) substr of s: s1<s2->-1,s1==s2->0,s1>s2->1
     int cmp_sub(int l1, int r1, int l2, int r2){ //[)
         auto sgn = [](int x){ return (x>0)-(x<0); };
         int len1 = r1-l1+1, len2 = r2-l2+1;
         return len_lcp(l1,l2) < min(len1,len2)
             ? sgn(sainv[l1]-sainv[l2]): sgn(len1-len2);
-    } //returns [) range of the suffix array, substr is given [)
+    }//returns [) range of the SA, substr is given [)
     pair<int, int> find_str(int s_l, int s_r) { //[)
         auto cmp = [&](int i, bool flip) -> bool {
             return flip ^ (len_lcp(i, s_l) < s_r - s_l); };

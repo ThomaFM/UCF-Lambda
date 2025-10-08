ad27fd00075ba1887aafd5020be2e6d175c38e1f
Thomas Meeks <thomasfm2004@gmail.com>
2025-10-08 16:23:47 -0400

    changed lazy segtree to be fast and modifiable (atcoder based)

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,71 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
+ * Author: Atcoder/Modified
+ * Date: 2025-10-08
  * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
+ * Description: Segment tree with lazy prop, modify at will
  * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
+ * Usage: lazy_segtree<ValType,LazyType> st(arr)
  * Status: stress-tested a bit
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
-
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
+template<class T, class F>
+struct lazy_segtree {
+	int N, log, S;
+	T idem = //modify: op(val,idem) = val
+	F defLazy = //modify: applyLazy/combLazy(val,defLazy) = val
+	vector<T> d;
+	vector<F> lz;
+	T op(T left, T right) { /*modify*/ }
+	T applyLazy(T val, F lazy) { /*modify*/ }
+	F combLazy(F old, F nw) { /*modify*/ } 
+	lazy_segtree(const vector<T>& v):
+		N(sz(v)), log(__lg(2 * N - 1)), S(1 << log), d(2 * S, idem),
+		lz(S, defLazy) {
+		for (int i = 0; i < N; i++) d[S + i] = v[i];
+		for (int i = S - 1; i >= 1; i--) pull(i);
 	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
+	void apply(int k, F f) {
+		d[k] = applyLazy(d[k], f); //len = S>>(31-__builtin_clz(k));
+		if (k < S) lz[k] = combLazy(lz[k],f);
 	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
+	void push(int k) {
+		apply(2 * k, lz[k]), apply(2 * k + 1, lz[k]), lz[k] = defLazy;
 	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
+	void push(int l, int r) {
+		int zl = __builtin_ctz(l), zr = __builtin_ctz(r);
+		for (int i = log; i > min(zl, zr); i--) {
+			if (i > zl) push(l >> i);
+			if (i > zr) push((r - 1) >> i);
 		}
 	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
+	void pull(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+	void set(int p, T x) {
+		p += S;
+		for (int i = log; i >= 1; i--) push(p >> i);
+		for (d[p] = x; p /= 2;) pull(p);
+	}
+	T query(int l, int r) {
+		if (l == r) return idem;
+		push(l += S, r += S);
+		T vl = idem, vr = idem;
+		for (; l < r; l /= 2, r /= 2) {
+			if (l & 1) vl = op(vl, d[l++]);
+			if (r & 1) vr = op(d[--r], vr);
 		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+		return op(vl, vr);
+	}
+	void update(int l, int r, F f) {
+		if (l == r) return;
+		push(l += S, r += S);
+		for (int a = l, b = r; a < b; a /= 2, b /= 2) {
+			if (a & 1) apply(a++, f);
+			if (b & 1) apply(--b, f);
+		}
+		int zl = __builtin_ctz(l), zr = __builtin_ctz(r);
+		for (int i = min(zl, zr) + 1; i <= log; i++) {
+			if (i > zl) pull(l >> i);
+			if (i > zr) pull((r - 1) >> i);
+		}
 	}
 };
